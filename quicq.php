bb601cad60999a4e76c902dcea5ce314e67e235b
Sander | Afosto <96471984+sanderkah@users.noreply.github.com>
2022-02-18 12:16:31 +0100

    Update quicq.php

--- quicq.php
+++ quicq.php
@@ -3,7 +3,7 @@
  * Plugin Name:    Quicq for WebP images
  * Plugin URI:     https://quicq.io/
  * Description:    Quicq integration for Wordpress.
- * Version:        1.3
+ * Version:        1.4
  * Author:        Afosto
  * Author URI:    https://afosto.com
  * Domain Path:   /languages
